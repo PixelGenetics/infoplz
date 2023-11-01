12bf75757261e21e01eea782dc26a1219a129ffe
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-31 20:38:54 -0600

    Making refactor

--- src/components/home/News/News.jsx
+++ src/components/home/News/News.jsx
@@ -5,15 +5,89 @@ const News = () =>{
     return (
         <>
         <div className='mainContainer'>
-            <div className='gridNoticias'>
+            <div className='cuerpo'>
+                <div className='imagen'>
                     <img src={example} alt="" />
-                    <h2>Automotive Parts & System</h2>
-                    <p>Tiling is an effective way to add 
-                        elegance & style to any bath or 
-                        kitchen Yellow Hats Remodeling Team
-                        specializes in tile installation 
-                        and works directly with each.</p>
-                    <a href="#">Read More</a>
+                </div>
+                <div className='gridNoticias'>
+                        <h2>Automotive Parts & System</h2>
+                        <p>Tiling is an effective way to add 
+                            elegance & style to any bath or 
+                            kitchen Yellow Hats Remodeling Team
+                            specializes in tile installation 
+                            and works directly with each.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className='cuerpo'>
+                <div className='imagen'>
+                    <img src={example} alt="" />
+                </div>
+                <div className='gridNoticias'>
+                        <h2>Automotive Parts & System</h2>
+                        <p>Tiling is an effective way to add 
+                            elegance & style to any bath or 
+                            kitchen Yellow Hats Remodeling Team
+                            specializes in tile installation 
+                            and works directly with each.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className='cuerpo'>
+                <div className='imagen'>
+                    <img src={example} alt="" />
+                </div>
+                <div className='gridNoticias'>
+                        <h2>Automotive Parts & System</h2>
+                        <p>Tiling is an effective way to add 
+                            elegance & style to any bath or 
+                            kitchen Yellow Hats Remodeling Team
+                            specializes in tile installation 
+                            and works directly with each.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className='cuerpo'>
+                <div className='imagen'>
+                    <img src={example} alt="" />
+                </div>
+                <div className='gridNoticias'>
+                        <h2>Automotive Parts & System</h2>
+                        <p>Tiling is an effective way to add 
+                            elegance & style to any bath or 
+                            kitchen Yellow Hats Remodeling Team
+                            specializes in tile installation 
+                            and works directly with each.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className='cuerpo'>
+                <div className='imagen'>
+                    <img src={example} alt="" />
+                </div>
+                <div className='gridNoticias'>
+                        <h2>Automotive Parts & System</h2>
+                        <p>Tiling is an effective way to add 
+                            elegance & style to any bath or 
+                            kitchen Yellow Hats Remodeling Team
+                            specializes in tile installation 
+                            and works directly with each.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className='cuerpo'>
+                <div className='imagen'>
+                    <img src={example} alt="" />
+                </div>
+                <div className='gridNoticias'>
+                        <h2>Automotive Parts & System</h2>
+                        <p>Tiling is an effective way to add 
+                            elegance & style to any bath or 
+                            kitchen Yellow Hats Remodeling Team
+                            specializes in tile installation 
+                            and works directly with each.</p>
+                        <a href="#">Read More</a>
+                </div>
             </div>
         </div>
         </>
